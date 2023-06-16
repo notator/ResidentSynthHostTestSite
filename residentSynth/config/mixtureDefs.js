0ac3d3b7ef14ac0f5dbe7a50eac4a659d6644b4d
notator <j.ingram@netcologne.de>
2023-06-16 09:52:09 +0200

    Reverted mixtureDefs.js

--- residentSynth/config/mixtureDefs.js
+++ residentSynth/config/mixtureDefs.js
@@ -3,9 +3,8 @@ console.log('load mixtureDefs.js');
 // This file can be omitted by applications that don't use mixtures.
 // The mixtureDefs array is an array of mixture definitions.
 // The maximum number of mixture definitions is 127.
-// Each mixture definition contains a name and an noteExtraNotes array
-// containing entries that are objects having note and extraNotes attributes.
-// Each extraNotes is an array containing arrays of the form [keyInterval, relativeVelocity].
+// Each mixture definition contains a name and an extraNotes array
+// containing entries of the form[keyInterval, relativeVelocity].
 // Each extraNotes array can have any length greater than 0.
 // If the [keyInterval, relativeVelocity] array is not _empty_,
 // it defines a new noteOn that will be sent with the original noteOn message.
@@ -15,98 +14,49 @@ console.log('load mixtureDefs.js');
 // relativeVelocity are usually floats > 0 and < 1, but can be <= 100.0.
 // (originalKey + keyInterval) will be silently coerced to the range 0..127 inclusive.
 // (originalvelocity * relativeVelocity) will be silently coerced to the range 1..127 inclusive.
-// If noteExtraNotes.note is undefined, its extraNotes array applies to all notes in the mixture,
-// except those having a defined noteExtraNotes.note in the mixture. 
 ResSynth.mixtureDefs =
     [
         {
             name: "none",
-            noteExtraNotes:
+            extraNotes:
                 [
-                    {
-                        note: undefined, // default for all notes not mentioned in the other noteExtraNotes
-                        extraNotes:
-                            [
-                                []
-                            ]
-                    }
+                    []
                 ]
         },
         {
             name: "2.1: (+19)",
-            noteExtraNotes:
+            extraNotes:
                 [
-                    {
-                        note: undefined, // default for all notes not mentioned in the other noteExtraNotes
-                        extraNotes:
-                            [
-                                [19, 0.5]
-                            ]
-                    },
-                    {
-                        note: 60, // extraNotes just for key 60
-                        extraNotes:
-                            [
-                                [4, 0.9]
-                            ]
-                    }
+                    [19, 0.5]
                 ]
         },
         {
             name: "4.1 (+4, 8, 13)",
-            noteExtraNotes:
+            extraNotes:
                 [
-                    {
-                        note: undefined, // default for all notes not mentioned in the other noteExtraNotes
-                        extraNotes:
-                            [
-                                [4, 0.75], [8, 0.5], [13, 0.4]
-                            ]
-                    }
+                    [4, 0.75], [8, 0.5], [13, 0.4]
                 ]
-
         },
         {
             name: "5.1: (+4, 7, 12, 24)",
-            noteExtraNotes:
+            extraNotes:
                 [
-                    {
-                        note: undefined, // default for all notes not mentioned in the other noteExtraNotes
-                        extraNotes:
-                            [
-                                [4, 0.75], [7, 0.5], [12, 0.4], [24, 0.2]
-                            ]
-                    }
+                    [4, 0.75], [7, 0.5], [12, 0.4], [24, 0.2]
                 ]
-
         },
         {
             name: "5.2 (+6, 12, 24, 37)",
-            noteExtraNotes:
+            extraNotes:
                 [
-                    {
-                        note: undefined, // default for all notes not mentioned in the other noteExtraNotes
-                        extraNotes:
-                            [
-                                [6, 0.75], [12, 0.4], [24, 0.2], [37, 0.1]
-                            ]
-                    }
+                    [6, 0.75], [12, 0.4], [24, 0.2], [37, 0.1]
                 ]
-
         },
         {
             name: "5.3 (+9, 13, 12, 24)",
-            noteExtraNotes:
+            extraNotes:
                 [
-                    {
-                        note: undefined, // default for all notes not mentioned in the other noteExtraNotes
-                        extraNotes:
-                            [
-                                [9, 0.4], [13, 0.3], [12, 0.2], [24, 0.1]
-                            ]
-                    }
+                    [9, 0.4], [13, 0.3], [12, 0.2], [24, 0.1]
                 ]
-
         }
     ];
 
