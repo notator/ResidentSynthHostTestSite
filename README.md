2faf09354bd42393217aeca21c5fe6845a71b59f
notator <j.ingram@netcologne.de>
2023-10-04 14:40:03 +0200

    Completed keyboardSplitDefs documentation comments...
    in both the file and the README.

--- README.md
+++ README.md
@@ -140,14 +140,24 @@ The MIDI PRESET command message will set the preset using the index in the bank'
 
 **`keyboardSplitDefs.js`**  
 
-If this file is missing, the default is for there to be no split. The host application decides the channel on which  messages will be sent.  
-Split definition strings can have up to 127 `<key>:<channel>;` substrings, separated by optional whitespace. The final ";" is optional.  
-Each `<key>` is a number in range 0..127. The first `<key>` must be `0`. `<key>` values must be in ascending order, and may not repeat.  
-Each `<channel>` is a number in range 0..15. These values can be in any order, and can repeat within the string.  
-The default (empty) string means that all keys send messages on the channel currently being edited.  
-The split defintion string is parsed from left to right.
-All keys greater than or equal to the `<key>` substring send on the substring's `<channel>` unless overridden by a substring further to the right.  
-Valid split definition strings are, for example, `""`, `"0:0; 42:1;"`, `"0:3; 40:1; 50:2;"`, `"0:0; 40:1; 50:2; 60:0; 72:5"` etc.
+This file can be omitted by applications that don't use split keyboards.  
+The keyboardSplitDefs array contains up to 127 keyboardSplit definition (=keyboardSplitDef) strings,  
+each of which contains up to 127 `<key>:<channel>;` substrings separated by whitespace.  
+An empty keyboardSplitDef means that there is no overriding keyboardSplit, so that the incoming  
+MIDI message's channel will be used unchanged. This is also the default used when this file does not exist.  
+The following restrictions are checked when this file is loaded into the ResidentSynth:  
+There are less than 128 keyboardSplitDef strings in the array.  
+Each `<key>` must be an integer in range 0..127. The first `<key>` must be 0, with the rest in ascending  
+order. `<key>` values may not repeat in the string.  
+Each `<channel>` is an integer in range 0..15. These values can be in any order, and repeat in the string.  
+The `:` and `;` characters must be present, except at the very end of each keyboardSplitDef.   
+Each keyboardSplitDef is parsed from left to right.  
+All keys greater than or equal to the `<key>` component of a `<key>:<channel>` substring send on the  
+substring's channel unless overridden by a substring further to the right.    
+Example keyboardSplitDef strings are:  
+`""`, `"0:0; 42:1;"`, `"0:3; 40:1; 50:2;"`, `"0:0; 40:1; 50:2; 60:1; 72:5"` etc.  
+The (non-standard MIDI) SET_KEYBOARD_SPLIT_ARRAY message sets the current keyboard split  
+configuration using its index in this array.
 
 <a id="mixtureDefs"/>
 
--- residentSynth/config/keyboardSplitDefs.js
+++ residentSynth/config/keyboardSplitDefs.js
@@ -1,28 +1,30 @@
 console.log('load keyboardSplitDefs.js');
 
-// This file can be omitted by applications that don't use keyboardSplits.
-// There can be up to 128 keyboardSplit definitions (=keyboardSplitDef) in the keyboardSplitDefs array, each of which
-// contains zero or more "<key>:<channel>;" strings separated by whitespace.
+// This file can be omitted by applications that don't use split keyboards.
+// The keyboardSplitDefs array contains up to 127 keyboardSplit definition (=keyboardSplitDef) strings,
+// each of which contains up to 127 `<key>:<channel>;` substrings separated by whitespace.
 // An empty keyboardSplitDef means that there is no overriding keyboardSplit, so that the incoming
-// MIDI message's channel will be used unchanged. This is the default when this file does not exist.
-// Otherwise, each keyboardSplitDef contains up to 127 "<key>:<channel>;" sub-strings separated by whitespace.
-// The following restrictions are checked when this file is loaded into the ResidentSynth using setPrivateChannelPerKeyArrays():
-// There are less than 128 keyboardSplitDef strings in this file.
-// Each <key> is a number in range 0..127. Key values must be in ascending order, and may not repeat.
-// Each <channel> is a number in range 0..15. These values can be in any order, and repeat in the string.
-// The ':' and ';' characters must be present, except at the very end of each keyboardSplitDef.
+// MIDI message's channel will be used unchanged. This is also the default used when this file does not exist.
+// The following restrictions are checked when this file is loaded into the ResidentSynth:
+// There are less than 128 keyboardSplitDef strings in the array.
+// Each `<key>` must be an integer in range 0..127. The first `<key>` must be 0, with the rest in ascending
+// order. `<key>` values may not repeat in the string.
+// Each `<channel>` is an integer in range 0..15. These values can be in any order, and repeat in the string.
+// The `:` and `;` characters must be present, except at the very end of each keyboardSplitDef.  
 // Each keyboardSplitDef is parsed from left to right.
-// All keys greater than or equal to the key component of a <key:channel> substring send on the
-// substring's channel unless overridden by a substring further to the right.
-// Valid keyboardSplitDef strings are "", "42:1;", "40:1; 50:2;", "40:1; 50:2; 60:1; 72:5" etc.
-// In the ResidentSynthHost, these strings are used to populate a select control.
+// All keys greater than or equal to the `<key>` component of a `<key>:<channel>` substring send on the
+// substring's channel unless overridden by a substring further to the right.  
+// Example keyboardSplitDef strings are:
+// `""`, `"0:0; 42:1;"`, `"0:3; 40:1; 50:2;"`, `"0:0; 40:1; 50:2; 60:1; 72:5"` etc. 
+// The (non-standard MIDI) SET_KEYBOARD_SPLIT_ARRAY message sets the current keyboard split configuration  
+// using its index in this array.
 ResSynth.keyboardSplitDefs =
     [
         "", // index 0, (use incoming channel)
         "0:0", // index 1 
         "0:0; 60:1", // index 2
         "0:0; 48:1; 60:0; 72:1", // index 3
-        "0:0; 48:1; 60:2; 72:3", // index 4
+        "0:0; 48:1; 60:2; 72:3" // index 4
     ];
 
 
